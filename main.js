6475e653f82ab191d6b476586117844dc4abda14
Marce Ifran Singh <64060640+marceloifran@users.noreply.github.com>
2022-03-29 22:25:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.hero',{delay:200})
+ScrollReveal().reveal('.card',{delay:300})
+ScrollReveal().reveal('.link',{delay:350})
+ScrollReveal().reveal('.contenedor-textos-main',{delay:350})
