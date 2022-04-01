b80f4a8e650cae55e42cef88be0d388e6eae3e96
Marce Ifran Singh <64060640+marceloifran@users.noreply.github.com>
2022-03-31 21:22:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ ScrollReveal().reveal('.hero',{delay:200})
 ScrollReveal().reveal('.card',{delay:300})
 ScrollReveal().reveal('.link',{delay:350})
 ScrollReveal().reveal('.contenedor-textos-main',{delay:350})
+
+
+/*MODA*/
